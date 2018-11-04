02a2af8f427befa48fec98d54dd081c5258a3d01
shalalalatuzki <2550662608@qq.com>
2018-11-04 12:28:55 +0800

    screen post-processing effects

---
+++ Assets/Scripts/Chapter12/BrightnessSaturationAndContrast2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessSturationAndContrast2 :PostEffectBase2 {
+	public Shader BriSatConShader;
+	private Material BriSatConMaterial;
+	public  Material material{
+		get{
+			BriSatConMaterial=CheckShaderAndCreateMaterial(BriSatConShader,BriSatConMaterial);
+			return BriSatConMaterial;
+		}
+	}
+	public float brightness=1.0f;
+	public float saturation=1.0f;
+	public float contrast=1.0f;
+
+	void OnRenderImage(RenderTexture src,RenderTexture dest)
+	{
+		if(material!=null){
+			material.SetFloat("_Brightness",brightness);
+			material.SetFloat("_Saturation",saturation);
+			material.SetFloat("_Contrast",contrast);
+			Graphics.Blit(src,dest,material);
+		}
+		else{
+			Graphics.Blit(src,dest);
+		}
+	}
+}
---
+++ Assets/Scripts/Chapter12/PostEffectBase2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class PostEffectBase2 : MonoBehaviour {
+
+	// Use this for initialization
+	protected void checkResources(){
+		bool isSupported=checkSupport();
+		if(isSupported==false){
+			NotSupport();
+		}
+	}
+	protected void NotSupport(){
+		enabled=false;
+	}
+	protected bool checkSupport(){
+		if(SystemInfo.supportsImageEffects==false||SystemInfo.supportsRenderTextures==false){
+				print("can't render image effect or textures");
+				return false;
+		}
+		return true;
+	}
+	protected void Start () {
+		checkResources();
+	}
+	
+	// Update is called once per frame
+	protected Material CheckShaderAndCreateMaterial(Shader shader,Material material){
+		if(shader==null){
+			return null;
+		}
+		if(shader.isSupported&&material&&material.shader==shader){
+			return material;
+		}
+		else{
+			material=new Material(shader);
+			material.hideFlags=HideFlags.DontSave;
+			if(material){
+				return material;
+			}	
+			return null;
+		}
+	}
+}
